7a522a720319d81b5a9f3490af2860a3c9c3ab13
Tema817 <temashatov@gmail.com>
2026-03-08 03:13:44 +0200

    Create page.tsx

---
+++ app/(private routes)/notes/[id]/page.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api/serverApi";
+import NoteDetailsClient from "./NoteDetails.client";
+
+interface NoteModalPageProps {
+  params: Promise<{ id: string }>;
+}
+
+export async function generateMetadata({ params }: NoteModalPageProps): Promise<Metadata> {
+  const { id } = await params;
+  const note = await fetchNoteById(id);
+  return {
+    title: `Note ${note.title}`,
+    description: `${note.content.slice(0, 30)}`,
+    openGraph: {
+      title: `Note ${note.title}`,
+      description: `${note.content.slice(0, 30)}`,
+      url: `https://08-zustand-budsk7974-temas-projects-820b2b53.vercel.app/notes/notes/${id}`,
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
+  };
+}
+
+export default async function NoteModalPage({ params }: NoteModalPageProps) {
+  const { id } = await params;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient id={id} />
+    </HydrationBoundary>
+  );
+}
